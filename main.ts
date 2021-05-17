9b2bd155b8a4505796a0f774c0c08f806b9d10d8
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-05-18 00:15:25 +0700

    literal types

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
 // union types
-function add(input1: number | string, input2: number | string) {
+function add(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-text" | "as-number"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAge = add(20, 30);
+const combinedAge = add(20, 30, "as-number");
 console.log(combinedAge);
 
-const mixingName = add("yo", "decan");
+const combinedStringAge = add("40", "50", "as-text");
+
+const mixingName = add("yo", "decan", "as-text");
 console.log(mixingName);
