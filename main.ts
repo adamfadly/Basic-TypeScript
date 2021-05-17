98f5950b49c6754200a16b82367e3f7ebc8b7117
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-05-17 20:15:56 +0700

    enums rules

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
 // declare array with type in TS
+enum ROLES {
+  ADMIN = 4,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
+  role: ROLES;
 } = {
   name: "joko",
   age: 30,
   hobbies: ["football", "touring"],
+  role: ROLES.AUTHOR,
 };
 
-console.log(person.hobbies);
+console.log(person.role);
