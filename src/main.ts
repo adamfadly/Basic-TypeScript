dd6ab6a6a2ff863b1c5df163bef3abf17690e765
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-05-31 23:53:37 +0700

    practicing deafult parameter function single or mutiple parameter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-function addNum(num1: number, num2: number) {
+//if Default function has two paramatesr always put default parameter on the second parameter otherwise will thrown an error
+function addNum(num1: number, num2: number = 10) {
   return num1 + num2;
 }
+// Default parameter only has one parameter
+function printString(name: string = "mandra") {
+  return name;
+}
 
-const minus = (num1: number, num2: number) => num1 - num2;
+const minus = (num1: number, num2: number = 2) => num1 - num2;
 
-console.log(addNum(1, 7));
+console.log(addNum(1));
 
-console.log(minus(2, 1));
+console.log(minus(1));
+
+console.log(printString("atun"));
